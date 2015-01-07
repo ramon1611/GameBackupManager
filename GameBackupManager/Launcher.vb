c7f264e7cb3f0b935fe8fd048121cdfb565e54d1
ramon1611 <rosin.ramon@gmail.com>
2015-01-07 19:58:36 +0100

    Alpha Version 0.0.0.1

---
+++ GameBackupManager/Launcher.vb
@@ -0,0 +1,57 @@
+﻿Namespace LauncherType
+	Public Class Launcher
+		Public Enum BackupMethod
+			Integrated = 0
+			Manual = 1
+		End Enum
+
+		Property ID As String
+		Property Name As String
+		Property Publisher As String
+		Property Disabled As Boolean
+		Property Detected As Boolean
+		Property Backup As BackupMethod
+
+		Property MainDirectory As String
+		Property MainExecutable As String
+		Property GameDirectory As String
+		Property AdditionalPaths As Dictionary(Of String, String)
+
+		Property StartArgument As String
+		Property ShutdownArgument As String
+		Property LaunchGameArgument As String
+		Property RestoreBackupArgument As String
+
+		Property StartIsStandalone As Boolean
+		Property ShutdownIsStandalone As Boolean
+		Property LaunchGameIsStandalone As Boolean
+		Property RestoreBackupIsStandalone As Boolean
+
+		Sub New()
+			ClearData()
+		End Sub
+
+		Public Sub ClearData()
+			ID = String.Empty
+			Name = String.Empty
+			Publisher = String.Empty
+			Disabled = Nothing
+			Detected = Nothing
+
+			MainDirectory = String.Empty
+			MainExecutable = String.Empty
+			GameDirectory = String.Empty
+			AdditionalPaths = New Dictionary(Of String, String)
+
+			StartArgument = String.Empty
+			ShutdownArgument = String.Empty
+			LaunchGameArgument = String.Empty
+			RestoreBackupArgument = String.Empty
+
+			StartIsStandalone = Nothing
+			ShutdownIsStandalone = Nothing
+			LaunchGameIsStandalone = Nothing
+			RestoreBackupIsStandalone = Nothing
+		End Sub
+	End Class
+End Namespace
---
+++ GameBackupManager/Settings.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Xml, System.Globalization, System.Threading, System.Resources
+Imports GameBackupManager.LauncherType
+
+Public Class Settings
+	Dim locRM As New ResourceManager("GameBackupManager.Strings", GetType(MainWindow).Assembly)
+	Dim availableLanguages As New List(Of Integer)
+
+	Private Sub Settings_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+		availableLanguages.Add(1031)
+		availableLanguages.Add(1033)
+
+		loadLanguageComboBox()
+	End Sub
+
+	Private Sub ApplyConfigButton_Click(sender As System.Object, e As System.EventArgs) Handles ApplyConfigButton.Click
+		My.Settings.Save()
+		My.Settings.Reload()
+		reloadLocalization()
+	End Sub
+
+	Private Sub CancelConfigButton_Click(sender As System.Object, e As System.EventArgs) Handles CancelConfigButton.Click
+		My.Settings.Reload()
+		Me.Close()
+	End Sub
+
+	Private Sub OkConfigButton_Click(sender As System.Object, e As System.EventArgs) Handles OkConfigButton.Click
+		My.Settings.Save()
+		My.Settings.Reload()
+		reloadLocalization()
+		Me.Close()
+	End Sub
+
+	Private Sub LanguageComboBox_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles LanguageComboBox.SelectedIndexChanged
+		My.Settings.defaultLanguage = CultureInfo.GetCultureInfo(LanguageComboBox.SelectedItem.ToString).LCID
+		Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(LanguageComboBox.SelectedItem.ToString)
+		Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(LanguageComboBox.SelectedItem.ToString)
+	End Sub
+
+	Public Sub loadLanguageComboBox()
+		LanguageComboBox.Items.Clear()
+
+		For Each languageItem As Integer In availableLanguages
+			LanguageComboBox.Items.Add(CultureInfo.GetCultureInfo(languageItem).DisplayName)
+		Next
+
+		LanguageComboBox.SelectedItem = CultureInfo.GetCultureInfo(My.Settings.defaultLanguage).DisplayName
+	End Sub
+
+	Public Sub reloadLocalization()
+		If MessageBox.Show(locRM.GetString("localizationReloadMessage"), locRM.GetString("localizationReloadMessage_caption"), MessageBoxButtons.YesNo, MessageBoxIcon.Information) = Windows.Forms.DialogResult.Yes Then
+			Application.Restart()
+		End If
+	End Sub
+End Class
